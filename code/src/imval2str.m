01064067fef51c68b02d7967a84872d0ca300df5
Mayankm96 <mayankmittal155@gmail.com>
2017-03-28 03:08:34 +0530

    circular matching implemented

---
+++ code/src/imval2str.m
@@ -0,0 +1,15 @@
+function dig = imval2str(i)
+%% This function converts the input number to a string of three characters. 
+% For instance, an input of '1' is returned as '001'
+
+if i<10
+    dig=strcat('00',num2str(i));
+elseif i<100
+    dig=strcat('0',num2str(i));
+elseif i<1000
+    dig=strcat(num2str(i));
+else
+    error('The data file too big to process further that is number >1000.');
+end
+end
+
